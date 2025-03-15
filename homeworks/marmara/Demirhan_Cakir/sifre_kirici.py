ba5e3794699ebee75b6b9b8697755b6718b3522b
Demirhan Çakır <demirhan4856@gmail.com>
2025-03-15 15:59:39 +0300

    Ödev tamamlandı - Demirhan Çakır

--- homeworks/marmara/Demirhan_Cakir/sifre_kirici.py
+++ homeworks/marmara/Demirhan_Cakir/sifre_kirici.py
@@ -7,53 +7,69 @@ class SifreKirici:
         secim = input("Ornek mesaji cozmek icin '1', Kendi mesajinizi cozmek icin '2': ")
         if secim == "1":
             sifre = self.sifre
-            alfabe = self.alfabe
-            sifre = sifre.lower()
-            sifre = sifre.split(" ")
-            sifreCoz = []
-            for kelime in sifre:
-                kelimeCoz = ""
-                for harf in kelime:
-                    if harf in alfabe:
-                        harfIndex = alfabe.index(harf)
-                        harfIndex -= 5
-                        if harfIndex < 0:
-                            harfIndex += len(alfabe)
-                        kelimeCoz += alfabe[harfIndex]
-                    else:
-                        kelimeCoz += harf
-                sifreCoz.append(kelimeCoz)
-            sifreCoz = ' '.join(sifreCoz)
-            return sifreCoz
-        
         elif secim == "2":
-            mesaj = input("Şifreli mesajı giriniz: ")
-            self.mesaj = mesaj
-            alfabe = self.alfabe
-            mesaj = mesaj.lower()
-            mesaj = mesaj.split(" ")
-            sifreCoz = []
-            for kelime in mesaj:
-                kelimeCoz = ""
-                for harf in kelime:
-                    if harf in alfabe:
-                        harfIndex = alfabe.index(harf)
-                        harfIndex -= 5
-                        if harfIndex < 0:
-                            harfIndex += len(alfabe)
-                        kelimeCoz += alfabe[harfIndex]
+            while True:
+                mesaj = input("Şifreli mesajı giriniz (sadece sayi ya da sadece harf giriniz!): ")
+                try:
+                    sayilar=0
+                    harfler=0   
+                    for kelime in mesaj.split(" "):
+                        if kelime.isdigit():
+                            sayilar=1
+                        elif kelime.isalpha():
+                            harfler=1
+                    if sayilar==1 and harfler==0 or sayilar==0 and harfler==1:
+                        sifre = mesaj
+                        break
                     else:
-                        kelimeCoz += harf
-                sifreCoz.append(kelimeCoz)
-            sifreCoz = ' '.join(sifreCoz)
-            return sifreCoz
+                        print("\nGirilen mesaj hem harf hem de rakam içeriyor. Tekrar deneyin lütfen.\n")
+                except Exception as e:
+                    print(f"Hata oluştu: {e}") 
+            
+        alfabe = self.alfabe
+        sifre = sifre.lower().split(" ")
+        sifreCoz = []
+        for kelime in sifre:
+            kelimeCoz = ""
+            for harf in kelime:
+                if harf in alfabe:
+                    harfIndex = alfabe.index(harf)
+                    harfIndex -= 5
+                    if harfIndex < 0:
+                        harfIndex += len(alfabe)
+                    kelimeCoz += alfabe[harfIndex]
+                else:
+                    kelimeCoz += harf
+            sifreCoz.append(kelimeCoz)
         
-    
+        if sifreCoz[-1].isdigit():
+            sifreCoz = sifreCoz[::-1]
+            ters_kelimeler = [kelime[::-1] for kelime in sifreCoz]
+            sifreCoz = ' '.join(ters_kelimeler)
+        else:
+            sifreCoz = ' '.join(sifreCoz)
+        return "Mesajin sifresi cozulmus hali --> ",sifreCoz
+        
     def mesajSifrelendir(self):
-        mesaj = input("Sifrelenecek mesaji giriniz: ")
+        while True:
+            mesaj = input("Şifrelenecek mesajı giriniz (sadece sayi ya da sadece harf giriniz!): ")
+            try:
+                sayilar=0
+                harfler=0   
+                for kelime in mesaj.split(" "):
+                    if kelime.isdigit():
+                        sayilar=1
+                    elif kelime.isalpha():
+                        harfler=1
+                if sayilar==1 and harfler==0 or sayilar==0 and harfler==1:
+                    sifre = mesaj
+                    break
+                else:
+                    print("\nGirilen mesaj hem harf hem de rakam içeriyor. Tekrar deneyin lütfen.\n")
+            except Exception as e:
+                print(f"Hata oluştu: {e}") 
         alfabe = self.alfabe    
-        mesaj = mesaj.lower()
-        mesaj = mesaj.split(' ')
+        mesaj = mesaj.lower().split(" ")
         sifre = []
         for kelime in mesaj:
             kelimeSifre = ""
@@ -67,8 +83,13 @@ class SifreKirici:
                 else:
                     kelimeSifre += harf
             sifre.append(kelimeSifre)
-        sifre = ' '.join(sifre)
-        return sifre
+        if sifre[-1].isdigit():
+            sifre = sifre[::-1]
+            ters_kelimeler = [kelime[::-1] for kelime in sifre] 
+            sifre = ' '.join(ters_kelimeler)
+        else:
+            sifre = ' '.join(sifre)
+        return "Mesajin sifrelenmis hali --> ",sifre
 
 
 sifrekirici = SifreKirici()
